70f824e4e5e99e529b52c997c2de857a22efd2d7
mishal <mish.al.shomary@gmail.com>
2023-04-06 17:05:53 +0300

    added logging

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
+from datetime import datetime
 from os import listdir, popen, mkdir, path
 
-def arrangeFiles():
+def arrangeFiles(verbose):
     distDir = 'languages'
     if not distDir in listdir('.'):
         mkdir(distDir)
@@ -9,19 +11,28 @@ def arrangeFiles():
     targetFiles = listdir(targetDir)
 
     for file in targetFiles:
-        language = file.split('-')[0]
-        distLanguages = listdir(distDir)                    # repeated with each file for integrity
+        try:
+            language = file.split('-')[0]
+            distLanguages = listdir(distDir)                    # repeated with each file for integrity
 
-        if not language in distLanguages:                   # create a language directory if it doesn't exist
-            languagePath = path.join(distDir, language)
-            mkdir(languagePath)
+            if not language in distLanguages:                   # create a language directory if it doesn't exist
+                languagePath = path.join(distDir, language)
+                mkdir(languagePath)
 
-        filePath = path.join(targetDir, file)               # file to copy
-        fileDist = path.join(distDir, language, file)       # distenation
+            filePath = path.join(targetDir, file)               # file to copy
+            fileDist = path.join(distDir, language, file)       # distenation
+            copyCmd = f'cp {filePath} {fileDist}'
+            popen(copyCmd)
 
-        copyCmd = f'cp {filePath} {fileDist}'
-        popen(copyCmd)
+        except Exception as e:
+            if verbose:
+                f = open("errLog.txt", "a")
+                time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                err = f'[{time}] {file} [ERR]: {str(e)}\n'
+                f.write(err)
+                f.close()
 
 
 if __name__ == '__main__':
-    arrangeFiles()
+    verbose = '-verbose' in sys.argv
+    arrangeFiles(verbose)
